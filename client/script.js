eec0e67307353ccb7c4e64cfc32d6e814793f68b
vincentoir <121857150+vincentoir@users.noreply.github.com>
2023-02-22 11:56:18 +0300

    update server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chat-bot6-0.onrender.com', {
+    const response = await fetch('https://vincent-oira.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
